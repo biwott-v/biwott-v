ec6e036369dfc3c3ea939d05a98d172a6901eff6
Immanuel <ipmaster40@gmail.com>
2025-04-22 18:45:23 +0300

    Added the sidebar and skill details

---
+++ skilllink-fe/src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2>Available Skills</h2>
+      {/* We’ll map skills here soon */}
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ skilllink-fe/src/components/SkillDetails.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SkillDetails() {
+  return (
+    <div className="skill-details">
+      <h2>Select a Skill</h2>
+      <p>The full skill or service details will appear here.</p>
+    </div>
+  );
+}
+
+export default SkillDetails;
