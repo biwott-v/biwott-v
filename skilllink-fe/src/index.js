28506a7eeb94a076bab7b9cc68d0834b7427dd54
Yego <eddieyego273@gmail.com>
2025-04-28 09:05:52 +0300

    API

--- skilllink-fe/src/index.js
+++ skilllink-fe/src/index.js
@@ -1,16 +1,15 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './Login';
-
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+  
   </React.StrictMode>
 );
 
